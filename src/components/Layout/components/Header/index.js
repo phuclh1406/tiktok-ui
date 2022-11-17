e712a46423c4abcb8ed66ba9c47cdfaae424e902
phuclh1406 <phuclh1406@gmail.com>
2022-11-17 13:49:11 +0700

    Xay dung UI Header #5

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -16,6 +16,21 @@ const MENU_ITEMS = [
   {
     icon: <FontAwesomeIcon icon={faEarthAsia} />,
     title: 'English',
+    children: {
+      title: 'Language',
+      data: [
+        {
+          type: 'language',
+          code: 'en',
+          title: 'English'
+        },
+        {
+          type: 'language', 
+          code: 'vi',
+          title: 'Tieng Viet'
+        }
+      ]
+    }
   },
   {
     icon: <FontAwesomeIcon icon={faCircleQuestion} />,
@@ -36,6 +51,15 @@ function Header() {
       setSearchResult([]);
     }, 0);
   }, []);
+
+  const handleMenuChagne = (menuItem) => {
+    switch (menuItem.type) {
+      case 'language':
+        ///Handle change language
+        break;
+      default:
+    }
+  }
 
   return (
     <header className={cx('wrapper')}>
@@ -73,7 +97,7 @@ function Header() {
           <Button text>Upload</Button>
           <Button primary >Log in</Button>
 
-          <Menu items={MENU_ITEMS}>
+          <Menu items={MENU_ITEMS} onChange={handleMenuChagne}>
             <button className={cx('more-btn')}>
               <FontAwesomeIcon icon={faEllipsisVertical} />
             </button>
--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -4,25 +4,54 @@ import {} from '~/components/Popper';
 import styles from './Menu.module.scss';
 import MenuItem from './MenuItem';
 import { Wrapper as PoperWrapper } from '~/components/Popper';
+import Header from './Header';
+import { useState } from 'react';
 
 const cx = classNames.bind(styles);
 
-function Menu({ children, items = [] }) {
+const defaultFn = () => {}
 
-    const renderItems = () => {
-        return items.map((item, index) => (
-            <MenuItem key={index} data={item} />    
-        ))
-    }
+function Menu({ children, items = [], onChange = defaultFn }) {
+  const [history, setHistory] = useState([{ data: items }]);
+
+  const current = history[history.length - 1];
+
+  const renderItems = () => {
+    return current.data.map((item, index) => {
+      const isParent = !!item.children;
+
+      return (
+        <MenuItem
+          key={index}
+          data={item}
+          onClick={() => {
+            if (isParent) {
+              setHistory((prev) => [...prev, item.children]);
+            } else {
+              onChange(item)
+            }
+          }}
+        />
+      );
+    });
+  };
 
   return (
     <Tippy
       interactive
-      delay = {[0, 700]}
+      delay={[0, 700]}
       placement="bottom-end"
       render={(attrs) => (
         <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
           <PoperWrapper className={cx('menu-poper')}>
+            {history.length > 1 && (
+              <Header
+                title="Language"
+                onBack={() => {
+                  setHistory((prev) => prev.slice(0, prev.length - 1));
+                }}
+              />
+            )}
             {renderItems()}
           </PoperWrapper>
         </div>
